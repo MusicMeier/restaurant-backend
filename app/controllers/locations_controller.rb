2a9cb328ad44a7c5c1e6b9c6c2abada11d47ac74
Music <musicm987@gmail.com>
2020-10-19 17:21:06 -0600

    added actions to controllers

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -1,2 +1,21 @@
 class LocationsController < ApplicationController
+
+    def index
+        @locations = Location.all
+
+        render json: @locations
+    end
+
+    def show
+        @location = Location.find(params[:id])
+
+        render json: @location
+    end
+
+    def create
+        @location = Location.create(
+            neighborhood: params[:id]
+        )
+        render json: @location
+        end
 end
--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,2 +1,28 @@
 class RestaurantsController < ApplicationController
+
+    def index
+        @restaurants = Restaurant.all
+
+        render json: @restaurants
+    end
+
+
+    def show
+        @restaurant = Restaurant.find(params[:id])
+
+        render json: @restaurant
+    end
+
+    def create
+        @newRestaurant = Restaurant.create(
+            name: params[:name],
+            logo: params[:logo],
+            rating: params[:rating],
+            price: params[:price],
+            location_id: params[:location_id],
+            cuisine_id: params[:cuisine_id]
+        )
+
+        render json: @newRestaurant
+    end
 end
